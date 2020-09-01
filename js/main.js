88aeb801f046dec560d107932743a0920edfa6d9
dom <domepiro87@yahoo.it>
2020-09-01 17:33:00 +0200

    fine

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
-// selezionare menu hamburger
+//  creare var per selezionare menu html (hamburger)
 var hamburgerMenu = $(".hamburger-menu");
 var hamburgerOpen = $(".header-right > a");
 var hamburgerClose = $(".hamburger-menu > a");
 
-//  .click per attivare l'evento al click
+//  .click per attivare il menu al click
 hamburgerOpen.click(
     function(){
         hamburgerMenu.show(500);
     }
 );
 
+//  .click per chiudere il menu al click
 hamburgerClose.click(
     function(){
         hamburgerMenu.hide(500);
